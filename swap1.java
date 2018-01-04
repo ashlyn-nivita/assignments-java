0076663e8ed35ac1c00147a44cabf30eaeefd296
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2018-01-04 21:36:41 +0530

    Create swap1.java

---
+++ swap1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class swap
+{  
+ public static void main(String args[])
+ {  
+  int a,b,temp;  
+  Scanner sc=new Scanner(System.in);
+  a=sc.nextInt();
+  b=sc.nextInt();
+  temp=a;
+  a=b;
+  b=temp;
+  System.out.println("a:"+a);
+  System.out.println("b:"+b);    
+ }  
+}  
