0fa66ffbb1d20be2aa36ea2b453482e37a006f36
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2018-01-04 21:27:43 +0530

    Create factorial_rc.java

---
+++ factorial_rc.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Factorial
+{  
+ static int factorial(int n)
+ {    
+  if (n == 0)    
+    return 1;    
+  else    
+    return(n * factorial(n-1));    
+ }    
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number;
+  Scanner sc=new Scanner(System.in);
+  number=sc.nextInt();
+  fact = factorial(number);   
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+} 
