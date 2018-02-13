65c5c685d528c387642df9008fc22249a7300efa
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2018-02-13 10:22:50 +0530

    Create nested.java

---
+++ nested.java
@@ -0,0 +1,45 @@
+package sample;
+public class NestingDemo 
+{
+	public static void main(String args[])
+	{
+	try
+    {
+		try
+		{
+			try
+			{
+				int arr[]= {1,2,3,4};
+				System.out.println(arr[0]);
+			}
+			catch(ArithmeticException k)
+			{
+				System.out.print("Arithmetic Exception");
+		        System.out.println(" handled in try-block3");
+		    }
+			}
+		    catch(ArithmeticException k2 )
+		    {
+		    	System.out.print("Arithmetic Exception");
+		     	System.out.println(" handled in main try-block");
+		    }
+		    catch(ArrayIndexOutOfBoundsException e)
+		    {
+		    	System.out.print("ArrayIndexOutOfBoundsException");
+		     	System.out.println(" handled in main try-block");
+		    }
+		    catch(Exception e5)
+		     {
+		    	System.out.print("Exception");
+		     	System.out.println(" handled in main try-block");
+		     }
+		  
+		}
+	finally
+	{
+		System.out.println("end");
+	}
+
+
+}
+}
